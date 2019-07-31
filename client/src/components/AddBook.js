2b12c86b244a77f5e4c527199f8daefaffa1b5b4
febanda01 <febanda91@gmail.com>
2019-07-31 01:56:32 -0500

    book-details

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { Component} from 'react';
 import {graphql, compose} from 'react-apollo'
 import {getAuthorsQuery, addBookMutation, getBooksQuery} from '../queries/queries'
 
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -2,18 +2,29 @@ import React from 'react'
 import {Component} from 'react'
 import {graphql} from 'react-apollo'
 import {getBooksQuery} from '../queries/queries'
- 
+
+
+//components
+import BookDetails from './BookDetails'
 
 
 
 class BookList extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            selected: null 
+        }
+    }
+
     displayBooks(){
         var data = this.props.data
         if(data.loading){
             return(<div>Loading books...</div>)
         }else{
             return data.books.map(book => {
-                return(<li key={book.id}>{book.name}</li>)
+                return(<li key={book.id} onClick={(e) => {this.setState({selected: book.id})}}>{book.name}</li>)
             })
         }
     }
@@ -24,6 +35,7 @@ class BookList extends Component{
                     {this.displayBooks()}
 
                 </ul>
+                <BookDetails bookid={this.state.selected}/>
             </div>
         )
     }
