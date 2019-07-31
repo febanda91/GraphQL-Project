11994490065bf29796e493c4d5082a79844f8239
febanda01 <febanda91@gmail.com>
2019-07-30 19:06:57 -0500

    front-end-added

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import BookList from './components/BookList'
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Francisco's Reading List</h1>
+      <BookList/>
+    </div>
+  );
+}
+
+export default App;
